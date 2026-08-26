d215e894ef7de9c6a24f7f60d92c9484c4432b69
uts_ <kcutsarga77@gmail.com>
2026-08-26 09:56:34 +0545

    updating login & register api with catch async

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -8,50 +8,46 @@ import { catchAsync } from "../utils/catchAsync.utils";
 
 
 // register
-export const register = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const {full_name, email, password, phone_number} = req.body;
-        if (!full_name) {
-            // const error = new Error("full name is required");
-            // error.status = "fail";
-            // error.statusCode = 400;
-            // error.success = false;
-            // throw error;
-            throw new AppError("full name is required", 400);
-        }
-        if (!email) {
-            throw new AppError("email is required", 400);
-        }
-        if (!password) {
-            throw new AppError("password is required", 400);
-        }
-        // create user instance
-        const user = new User({full_name: full_name, email, phone_number, password});
-        // hash password
-        const hash = await hashPassword(password);
-        user.password = hash;
-
-        // upload profile image
-
-        // save user
-        await user.save();
-        // convert user doc to obj and destructure
-        const {password: _, ...rest} = user.toObject();
-
-        // send success response
-        sendResponse(res, {
-            message: "Account Created",
-            statusCode: 201,
-            data: rest,
-        });
-    } catch (error) {
-        next(error);
+export const register = catchAsync(async (req, res) => {
+    const {full_name, email, password, phone_number} = req.body;
+    if (!full_name) {
+        // const error = new Error("full name is required");
+        // error.status = "fail";
+        // error.statusCode = 400;
+        // error.success = false;
+        // throw error;
+        throw new AppError("full name is required", 400);
     }
-};
+    if (!email) {
+        throw new AppError("email is required", 400);
+    }
+    if (!password) {
+        throw new AppError("password is required", 400);
+    }
+    // create user instance
+    const user = new User({full_name: full_name, email, phone_number, password});
+    // hash password
+    const hash = await hashPassword(password);
+    user.password = hash;
+
+    // upload profile image
+
+    // save user
+    await user.save();
+    // convert user doc to obj and destructure
+    const {password: _, ...rest} = user.toObject();
+
+    // send success response
+    sendResponse(res, {
+        message: "Account Created",
+        statusCode: 201,
+        data: rest,
+    });
+});
+
 // login
-export const login = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const {email, password} = req.body;
+export const login = catchAsync(async(req, res) => {
+    const {email, password} = req.body;
         if (!email) throw new AppError("email is required", 400);
         if (!password) throw new AppError("password is required", 400);
 
@@ -74,10 +70,7 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
             statusCode: 201,
             data: rest,
         });
-    } catch (error) {
-        next(error);
-    }
-};
+});
 // change password
 export const changePassword = catchAsync(async(req,res) => {
     const {old_password, new_password, id} = req.body;
